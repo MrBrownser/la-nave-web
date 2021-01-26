95e791f45f8f7789bc59f31dcb4e553d4931980e
Adria Castany <adria.castany@haufe.com>
2021-01-27 00:48:09 +0100

    Update icon and manifest file for displaying right info when installing in browser

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
+        name: `Entrenos La Nave`,
+        short_name: `La-Nave`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
