632b29c644bac82af47586e72050ba737ad014ca
Adria Castany <adria.castany@haufe.com>
2021-01-27 00:30:26 +0100

    Add Firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: '#',
-  authDomain: '#',
-  databaseURL: '#',
-  projectId: '#',
-  storageBucket: '#',
-  messagingSenderId: '#',
-  appId: '#',
+  apiKey: "AIzaSyBDR19yOjk6QK6TLt0RmuAfkRhO0usY-_g",
+  authDomain: "entrenos-la-nave.firebaseapp.com",
+  databaseURL: "https://entrenos-la-nave-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "entrenos-la-nave",
+  storageBucket: "entrenos-la-nave.appspot.com",
+  messagingSenderId: "974522665521",
+  appId: "1:974522665521:web:78edf4b292917f533ba310"
 };
